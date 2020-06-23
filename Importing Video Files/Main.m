97b15fcc2c277098e88f499a8616bb72d26bad16
Michael <67344799+MichaelTr7@users.noreply.github.com>
2020-06-23 18:00:58 -0400

    Update Main.m

--- Importing Video Files/Main.m
+++ Importing Video Files/Main.m
@@ -3,10 +3,11 @@
 File_Name = "Animated_Blocks_Test.avi";
 [Video_Structure,Frame_Data] = Import_Frames(File_Name);
 
-
+%Creating a montage of all video frames%
 montage(Frame_Data,'ThumbnailSize',[100, Inf]);
 title('Image Database Collection');
 
+%Playing the frames stored in the structure%
 implay(Video_Structure);
 
 
